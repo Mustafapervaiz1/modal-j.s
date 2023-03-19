cc61489700836f26ed0cb0ffa484f6fd1867e28a
mustafapervez <syalani.training1@gmail.com>
2023-03-19 16:00:38 +0500

    update but in process

---
+++ ui,ux/main.js
@@ -0,0 +1,14 @@
+let modalbtn1 = document.getElementById('#modalbtn1');
+// var btn2 = document.querySelector('#btn2')
+// var btn3 = document.querySelector('#btn3')
+
+let cover = document.getElementById('#cover');
+let active = document.querySelectorAll('.active');
+
+ modalbtn1.addEventListener("click", function () {
+//   console.log("my button is working")
+
+active.classList.add ("active");
+});
+
+  /*                       modalbtn2           start         */
