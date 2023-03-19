174645caa1b96268b3d183a58770cd603a91326c
mustafapervez <syalani.training1@gmail.com>
2023-03-19 23:36:47 +0500

    complete modal

--- ui,ux/main.js
+++ ui,ux/main.js
@@ -1,14 +1,26 @@
 let modalbtn1 = document.getElementById('modalbtn1');
-// var btn2 = document.querySelector('#btn2')
-// var btn3 = document.querySelector('#btn3')
+let modalbtn2 = document.getElementById('modalbtn2');
+let modalbtn3 = document.getElementById('modalbtn3');
 
-let cover = document.getElementById('#cover');
-let active = document.querySelectorAll('.active');
 
- modalbtn1.addEventListener("click", function () {
-//   console.log("my button is working")
+let cover = document.getElementById('cover');
 
-cover.classList.remove(".active");
+modalbtn1.addEventListener("click", function () {
+  cover.classList.toggle('hide')
 });
 
+
+modalbtn2.addEventListener("click", function () {
+  cover.classList.toggle('hide')
+});
+
+
+modalbtn3.addEventListener("click", function () {
+  cover.classList.toggle('hide')
+});
+
+
+document.getElementById('close_btn').addEventListener('click', (e) =>{
+  cover.classList.add('hide')
+})
   /*                       modalbtn2           start         */
